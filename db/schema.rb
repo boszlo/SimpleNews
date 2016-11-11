7842910d69315c7ee40680ea5978720552a98167
boszlo <maciej.x.deptula@gmail.com>
2016-11-11 14:53:45 +0000

    ratyrate models and files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110112747) do
+ActiveRecord::Schema.define(version: 20161111141846) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,23 @@ ActiveRecord::Schema.define(version: 20161110112747) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authorships", force: :cascade do |t|
+    t.integer  "author_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_authorships_on_article_id"
+    t.index ["author_id"], name: "index_authorships_on_author_id"
   end
 
   create_table "average_caches", force: :cascade do |t|
