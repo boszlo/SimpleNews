9a203845a373b9cddcc0a22a655de11ebf1035a7
boszlo <maciej.x.deptula@gmail.com>
2016-11-11 15:08:44 +0000

    removed duped migration for categoryid

--- db/migrate/20161109172834_fix_categoryid.rb
+++
@@ -1,7 +0,0 @@
-class FixCategoryid < ActiveRecord::Migration[5.0]
-  def change
-    change_table :articles do |t|
-      t.rename :categoryid, :category_id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -23,23 +23,6 @@ ActiveRecord::Schema.define(version: 20161111141846) do
     t.integer  "category_id"
   end
 
-  create_table "authors", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "authorships", force: :cascade do |t|
-    t.integer  "author_id"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_authorships_on_article_id"
-    t.index ["author_id"], name: "index_authorships_on_author_id"
-  end
-
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
     t.string   "rateable_type"
