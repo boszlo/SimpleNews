ce878399397b0c657fc63f3544fb6a68b0586be2
boszlo <maciej.x.deptula@gmail.com>
2016-11-13 20:13:22 +0000

    Added authors

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :category
+  has_many :authorships
+  has_many :authors, through: :authorships
   validates_presence_of [:body, :title, :readcount]
   ratyrate_rateable "rating"
 end
