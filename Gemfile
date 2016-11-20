899c0f95a2532d3861b6310b470c596e1b168e78
boszlo <maciej.x.deptula@gmail.com>
2016-11-20 23:01:39 +0000

    seed generated with forgery gem

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem "omniauth-google-oauth2"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'forgery', '0.6.0' #for seed data generation
   gem 'byebug', platform: :mri
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-authors_list = [
-  [ "Maciej", "Deptula", "Chambray vice viral kombucha celiac, dreamcatcher cray. Yuccie viral health goth, lomo meditation leggings twee disrupt franzen portland everyday carry trust fund. Gentrify knausgaard before they sold out, whatever snackwave photo booth vice banjo venmo shoreditch you probably haven't heard of them hexagon meggings locavore VHS. Franzen single-origin coffee poutine hell of ennui. Sartorial hexagon vinyl, aesthetic cronut kale chips trust fund chia 90's cardigan la croix direct trade. Umami air plant master cleanse farm-to-table, live-edge whatever hella brunch cold-pressed. Aesthetic glossier wayfarers affogato swag subway tile, etsy four dollar toast." ],
-  [ "Umberto", "Eco", "Artisan beard pabst, prism four dollar toast tote bag fam quinoa kinfolk messenger bag seitan intelligentsia. Copper mug jean shorts microdosing, tumeric gochujang chia umami yr chillwave blue bottle prism occupy. Bespoke skateboard gluten-free tbh, stumptown freegan raw denim keytar food truck beard copper mug ugh fingerstache. Celiac pinterest gastropub, pabst distillery four dollar toast pickled occupy edison bulb. Chicharrones kinfolk pug, woke glossier gochujang lo-fi affogato. Messenger bag hammock intelligentsia, cliche chicharrones banjo meggings. Knausgaard craft beer paleo, health goth shoreditch banh mi tbh prism dreamcatcher biodiesel narwhal freegan aesthetic." ],
-  [ "Xavier", "Martinez", "Humblebrag heirloom photo booth, activated charcoal coloring book four loko biodiesel austin occupy distillery pickled roof party butcher bespoke. Four loko readymade 8-bit raclette occupy next level, swag sriracha mumblecore scenester snackwave squid lumbersexual truffaut. Food truck leggings cray, chia synth tbh vinyl jianbing. Hoodie man braid thundercats YOLO, pinterest tumblr yr dreamcatcher sustainable cold-pressed raw denim. DIY bushwick williamsburg, wayfarers truffaut live-edge locavore 90's copper mug woke kale chips godard gluten-free umami twee. Ramps etsy beard man braid keffiyeh austin. Tumblr everyday carry offal man bun." ],
-  [ "Samantha", "Fox", "Health goth disrupt pabst before they sold out, four loko lomo typewriter kogi slow-carb YOLO chillwave ennui. Pickled neutra hot chicken ennui hammock, hell of irony locavore cray +1 vice sriracha gochujang sartorial. Echo park hella plaid raw denim everyday carry deep v mixtape. +1 tilde knausgaard cronut drinking vinegar, etsy vice shabby chic meh everyday carry. +1 locavore mustache franzen tote bag. Kogi wayfarers vexillologist pour-over, iceland pinterest VHS waistcoat poutine bushwick four dollar toast pabst mlkshk glossier before they sold out. Beard mixtape banh mi, pickled etsy gentrify four dollar toast retro deep v tumblr affogato." ],
-  [ "Angelica", "Hudson", "Health goth disrupt pabst before they sold out, four loko lomo typewriter kogi slow-carb YOLO chillwave ennui. Pickled neutra hot chicken ennui hammock, hell of irony locavore cray +1 vice sriracha gochujang sartorial. Echo park hella plaid raw denim everyday carry deep v mixtape. +1 tilde knausgaard cronut drinking vinegar, etsy vice shabby chic meh everyday carry. +1 locavore mustache franzen tote bag. Kogi wayfarers vexillologist pour-over, iceland pinterest VHS waistcoat poutine bushwick four dollar toast pabst mlkshk glossier before they sold out. Beard mixtape banh mi, pickled etsy gentrify four dollar toast retro deep v tumblr affogato." ]
-]
 
-authors_list.each do |firstname, lastname, bio|
-  Author.create( firstname: firstname, lastname: lastname, bio: bio )
+(0..15).each do |index|
+  Author.create( firstname: Forgery('name').first_name, lastname: Forgery('name').last_name, bio: Forgery('lorem_ipsum').paragraph )
 end
 
 Category.create(category_name: "News")
 Category.create(category_name: "Features")
 
-article_list = [
-  ["About life","Humblebrag heirloom photo booth, activated charcoal coloring book four loko biodiesel austin occupy distillery pickled roof party butcher bespoke. Four loko readymade 8-bit raclette occupy next level, swag sriracha mumblecore scenester snackwave squid lumbersexual truffaut. Food truck leggings cray, chia synth tbh vinyl jianbing. Hoodie man braid thundercats YOLO, pinterest tumblr yr dreamcatcher sustainable cold-pressed raw denim. DIY bushwick williamsburg, wayfarers truffaut live-edge locavore 90's copper mug woke kale chips godard gluten-free umami twee. Ramps etsy beard man braid keffiyeh austin. Tumblr everyday carry offal man bun.",true,0,0,1],
-  ["Thoughts on procrastation","Humblebrag heirloom photo booth, activated charcoal coloring book four loko biodiesel austin occupy distillery pickled roof party butcher bespoke. Four loko readymade 8-bit raclette occupy next level, swag sriracha mumblecore scenester snackwave squid lumbersexual truffaut. Food truck leggings cray, chia synth tbh vinyl jianbing. Hoodie man braid thundercats YOLO, pinterest tumblr yr dreamcatcher sustainable cold-pressed raw denim. DIY bushwick williamsburg, wayfarers truffaut live-edge locavore 90's copper mug woke kale chips godard gluten-free umami twee. Ramps etsy beard man braid keffiyeh austin. Tumblr everyday carry offal man bun.",false,0,0,2],
-  ["Bumburak","Humblebrag heirloom photo booth, activated charcoal coloring book four loko biodiesel austin occupy distillery pickled roof party butcher bespoke. Four loko readymade 8-bit raclette occupy next level, swag sriracha mumblecore scenester snackwave squid lumbersexual truffaut. Food truck leggings cray, chia synth tbh vinyl jianbing. Hoodie man braid thundercats YOLO, pinterest tumblr yr dreamcatcher sustainable cold-pressed raw denim. DIY bushwick williamsburg, wayfarers truffaut live-edge locavore 90's copper mug woke kale chips godard gluten-free umami twee. Ramps etsy beard man braid keffiyeh austin. Tumblr everyday carry offal man bun.",false,0,0,2],
-  ["Lost in this world alone","Humblebrag heirloom photo booth, activated charcoal coloring book four loko biodiesel austin occupy distillery pickled roof party butcher bespoke. Four loko readymade 8-bit raclette occupy next level, swag sriracha mumblecore scenester snackwave squid lumbersexual truffaut. Food truck leggings cray, chia synth tbh vinyl jianbing. Hoodie man braid thundercats YOLO, pinterest tumblr yr dreamcatcher sustainable cold-pressed raw denim. DIY bushwick williamsburg, wayfarers truffaut live-edge locavore 90's copper mug woke kale chips godard gluten-free umami twee. Ramps etsy beard man braid keffiyeh austin. Tumblr everyday carry offal man bun.",false,0,0,2],
-  ["If you ever wondered why","Humblebrag heirloom photo booth, activated charcoal coloring book four loko biodiesel austin occupy distillery pickled roof party butcher bespoke. Four loko readymade 8-bit raclette occupy next level, swag sriracha mumblecore scenester snackwave squid lumbersexual truffaut. Food truck leggings cray, chia synth tbh vinyl jianbing. Hoodie man braid thundercats YOLO, pinterest tumblr yr dreamcatcher sustainable cold-pressed raw denim. DIY bushwick williamsburg, wayfarers truffaut live-edge locavore 90's copper mug woke kale chips godard gluten-free umami twee. Ramps etsy beard man braid keffiyeh austin. Tumblr everyday carry offal man bun.",false,0,0,2]
-
-]
-
-article_list.each do |title, body, featured, readcount, rating, category_id|
-  Article.create(title: title, body: body, featured: featured, readcount: readcount, rating: rating, category_id: category_id)
+(0..15).each do |index|
+  featured = index.odd? ? true : false
+  category_id = index.odd? ? 1 : 2
+  Article.create(title: Forgery('lorem_ipsum').title, body: Forgery('lorem_ipsum').paragraphs, featured: featured, readcount: 0, rating: 0, category_id: category_id)
 end
 
 
 # for each article find unique image
-photos = {}
+photos = Dir.glob('app/assets/images/seed-images/*.jpg').compact
 
 Article.all.each do |article|
-  Dir.glob('app/assets/images/seed-images/*.jpg').each do |jpg|
-    next if photos.key?(article.id)
-    next if photos.has_value?(jpg)
-    photos[article.id] = jpg
-    article.photo = File.open(jpg)
+    random = Random.new.rand(0...photos.size)
+    jpg = photos[random]
+    article.photo = File.open(jpg) 
     article.save!
-  end
 end
 
 # add authors
@@ -57,7 +40,6 @@ end
 # add second author to first article
 Article.first.authors << Author.last
 
-puts photos
 puts "Seed planted!"
 
 
